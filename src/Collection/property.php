4c1e0f490ba0f813fb3fa985d1a7f754b4bb8d29
Mike Petrovich <mike@nextbigsound.com>
2015-07-11 02:58:40 -0400

    Accept getter functions for path parameter to Collection\property

--- src/Collection/property.php
+++ src/Collection/property.php
@@ -5,8 +5,9 @@ namespace Dash\Collection;
 /**
  * Creates a function that returns the value at a path on a collection.
  *
- * @param string $path Path of the property to retrieve; can be nested by
- *        delimiting each sub-property or array index with a period
+ * @param string|function $path Path of the property to retrieve; can be nested
+ *        by delimiting each sub-property or array index with a period. If it
+ *        is a function, the same function is returned.
  * @param mixed $default Default value to return if nothing exists at $path
  *
  * @return function Function that accepts a collection and returns the value
@@ -39,6 +40,11 @@ namespace Dash\Collection;
  */
 function property($path, $default = null)
 {
+	if (is_callable($path)) {
+		// $path is already a getter function
+		return $path;
+	}
+
 	$getter = function($value) use ($path, $default) {
 		// Checks for a direct element or property with the same name as $path
 		if (is_array($value) && array_key_exists($path, $value)) {
