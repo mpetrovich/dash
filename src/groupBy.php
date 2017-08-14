bae8c920fdff0903588aeed29cd21786d3f1faab
Mike Petrovich <michael.c.petrovich@gmail.com>
2017-08-14 00:11:50 -0400

    Add default value to groupby()
    for null \$groupBy values

--- src/groupBy.php
+++ src/groupBy.php
@@ -2,12 +2,13 @@
 
 namespace Dash;
 
-function groupBy($input, $groupBy)
+function groupBy($input, $groupBy, $defaultGroup = null)
 {
 	assertType($input, ['iterable']);
 
-	return reduce($input, function($grouped, $value) use ($groupBy) {
+	return reduce($input, function($grouped, $value) use ($groupBy, $defaultGroup) {
 		$key = get($value, $groupBy);
+		$key = is_null($key) ? $defaultGroup : $key;
 		$grouped[$key][] = $value;
 		return $grouped;
 	});
