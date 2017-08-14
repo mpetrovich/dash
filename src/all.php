6c4a6cdaad3eb4778bdb8839ada3f6d119ab695e
Mike Petrovich <michael.c.petrovich@gmail.com>
2017-08-13 23:37:19 -0400

    Add docs for all()

--- src/all.php
+++ src/all.php
@@ -2,6 +2,22 @@
 
 namespace Dash;
 
+/**
+ * Checks whether $predicate returns truthy for every item in $input.
+ * $predicate will be called with ($value, $key).
+ *
+ * @category Collection
+ * @param mixed $input Any iterable
+ * @param callable $predicate
+ * @return bool true if $predicate returns truthy for every item in $input
+ *
+ * @example
+	all([1, 2, 3], function($n) { return $n < 3; });  // === false
+	all([1, 2, 3], function($n) { return $n < 4; });  // === true
+
+	all([1, 2, 3], 'Dash\isOdd');  // === false
+	all([1, 3, 5], 'Dash\isOdd');  // === true
+ */
 function all($input, $predicate)
 {
 	return every($input, $predicate);
