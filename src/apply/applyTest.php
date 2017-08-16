e026565411e5eb7d878bd3533d1f570c8a1b05e2
Mike Petrovich <michael.c.petrovich@gmail.com>
2017-08-16 16:30:48 -0400

    Add tests for call(), apply()

--- src/apply/applyTest.php
+++ src/apply/applyTest.php
@@ -5,8 +5,34 @@
  */
 class applyTest extends PHPUnit_Framework_TestCase
 {
-	public function test()
+	/**
+	 * @dataProvider cases
+	 */
+	public function test($args, $expected)
 	{
-		$this->markTestIncomplete();
+		$func = function () use ($args) {
+			$this->assertEquals($args, func_get_args());
+			return Dash\sum(func_get_args());
+		};
+
+		$this->assertEquals($expected, Dash\apply($func, $args));
+	}
+
+	public function cases()
+	{
+		return [
+			'With no args' => [
+				'args' => [],
+				'expected' => 0,
+			],
+			'With one arg' => [
+				'args' => [3],
+				'expected' => 3,
+			],
+			'With multiple args' => [
+				'args' => [1, 2, 3, 4],
+				'expected' => 10,
+			],
+		];
 	}
 }
--- src/call/callTest.php
+++ src/call/callTest.php
@@ -5,8 +5,33 @@
  */
 class callTest extends PHPUnit_Framework_TestCase
 {
-	public function test()
+	public function testWithNoArgs()
 	{
-		$this->markTestIncomplete();
+		$func = function () {
+			$this->assertEquals([], func_get_args());
+			return Dash\sum(func_get_args());
+		};
+
+		$this->assertEquals(0, Dash\call($func));
+	}
+
+	public function testWithOneArgs()
+	{
+		$func = function () {
+			$this->assertEquals([3], func_get_args());
+			return Dash\sum(func_get_args());
+		};
+
+		$this->assertEquals(3, Dash\call($func, 3));
+	}
+
+	public function testWithSeveralArgs()
+	{
+		$func = function () {
+			$this->assertEquals([1, 2, 3, 4], func_get_args());
+			return Dash\sum(func_get_args());
+		};
+
+		$this->assertEquals(10, Dash\call($func, 1, 2, 3, 4));
 	}
 }
