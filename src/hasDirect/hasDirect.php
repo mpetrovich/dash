69321cb9ecbbc1ae27b888afb7aaa4730888ba9b
Mike Petrovich <michael.c.petrovich@gmail.com>
2017-08-18 22:33:52 -0400

    Add tests for hasDirect()

--- src/hasDirect/hasDirect.php
+++ src/hasDirect/hasDirect.php
@@ -2,8 +2,23 @@
 
 namespace Dash;
 
-function hasDirect($subject, $field)
+/**
+ * Checks whether a value has a direct child field.
+ *
+ * @param mixed $input
+ * @param string $field Name of the field
+ * @return boolean
+ *
+ * @example
+	hasDirect(['a' => ['b' => 1, 'c' => 2], 'a');  // === true
+	hasDirect(['a' => ['b' => 1, 'c' => 2], 'b');  // === false
+ *
+ * @example
+	hasDirect((object) ['a' => 1, 'b' => 2], 'b');  // === true
+ */
+function hasDirect($input, $field)
 {
-	return is_array($subject) && array_key_exists($field, $subject)
-		|| is_object($subject) && property_exists($subject, $field);
+	return is_array($input) && array_key_exists($field, $input)
+		|| is_object($input) && property_exists($input, $field)
+		|| $input instanceof \ArrayAccess && $input->offsetExists($field);
 }
--- src/hasDirect/hasDirectTest.php
+++ src/hasDirect/hasDirectTest.php
@@ -5,8 +5,72 @@
  */
 class hasDirectTest extends PHPUnit_Framework_TestCase
 {
-	public function test()
+	/**
+	 * @dataProvider cases
+	 */
+	public function test($input, $field, $expected)
 	{
-		$this->markTestIncomplete();
+		$this->assertEquals($expected, Dash\hasDirect($input, $field));
+	}
+
+	public function cases()
+	{
+		return [
+			'With null' => [
+				'input' => null,
+				'field' => 'foo',
+				'expected' => false,
+			],
+			'With a number' => [
+				'input' => 123.45,
+				'field' => 'foo',
+				'expected' => false,
+			],
+			'With a string' => [
+				'input' => 'hello',
+				'field' => 'foo',
+				'expected' => false,
+			],
+			'With an non-stdClass object' => [
+				'input' => new DateTime(),
+				'field' => 'getTimestamp',
+				'expected' => false,
+			],
+			'With an empty array' => [
+				'input' => [],
+				'field' => 0,
+				'expected' => false,
+			],
+			'With an indexed array' => [
+				'input' => [2, 3, 5, 8],
+				'field' => 2,
+				'expected' => true,
+			],
+			'With an associative array' => [
+				'input' => ['a' => 1, 'b' => 2, 'c' => 3],
+				'field' => 'b',
+				'expected' => true,
+			],
+			'With an empty stdClass' => [
+				'input' => (object) [],
+				'field' => 'b',
+				'expected' => false,
+			],
+			'With an stdClass' => [
+				'input' => (object) ['a' => 1, 'b' => 2, 'c' => 3],
+				'field' => 'b',
+				'expected' => true,
+			],
+			'With an empty ArrayObject' => [
+				'input' => new ArrayObject([]),
+				'field' => 'b',
+				'expected' => false,
+			],
+			'With an ArrayObject' => [
+				'input' => new ArrayObject(['a' => 1, 'b' => 2, 'c' => 3]),
+				'field' => 'b',
+				'expected' => true,
+			],
+		];
 	}
 }
