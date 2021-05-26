9a37fd9575c3e5db004eed60b04605e2d5998c0a
Hadi Zahedian <zahedianhadi@gmail.com>
2021-05-26 19:04:56 +0430

    [BUG] Fixed php8 error when calling hasDirect whit non-object or non-class variable as first argument.

--- src/hasDirect.php
+++ src/hasDirect.php
@@ -30,8 +30,7 @@ function hasDirect($input, $key)
 		return false;
 	}
 
-	return is_array($input) && array_key_exists($key, $input)
-		|| is_object($input) && property_exists($input, $key)
-		|| $input instanceof \ArrayAccess && $input->offsetExists($key)
-		|| method_exists($input, $key);
+    return is_array($input) && array_key_exists($key, $input)
+        || is_object($input) && (property_exists($input, $key) || method_exists($input, $key))
+        || $input instanceof \ArrayAccess && $input->offsetExists($key);
 }
